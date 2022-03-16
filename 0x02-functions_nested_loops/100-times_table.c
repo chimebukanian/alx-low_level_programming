59476bffdce32aa3deaf9a5d08db9fb7c97c926e
chimebukanian <chimebukaanyanwu@gmail.com>
2022-03-16 14:56:44 -0700

    s

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,15 +1,47 @@
 #include <stdio.h>
 
 /**
- * _islower - Check for lowercase character.
- * @c: Charater to check
- * Return: 1 if c is lowercase, return 0 otherwise.
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
  */
-
-int _islower(int c)
+void print_times_table(int n)
 {
-	if (c >= 'a' && c <= 'z')
-		return (1);
-	else
-		return (0);
+	int i, j, k;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				k = j * i;
+				if (j == 0)
+				{
+					putchar(k + '0');
+				} else if (k < 10 && j != 0)
+				{
+					putchar(',');
+					putchar(' ');
+					putchar(' ');
+					putchar(' ');
+					putchar(k + '0');
+				} else if (k >= 10 && k < 100)
+				{
+					putchar(',');
+					putchar(' ');
+					putchar(' ');
+					putchar((k / 10) + '0');
+					putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					putchar(',');
+					putchar(' ');
+					putchar((k / 100) + '0');
+					putchar(((k / 10) % 10) + '0');
+					putchar((k % 10) + '0');
+				}
+			}
+			putchar('\n');
+		}
+	}
 }
