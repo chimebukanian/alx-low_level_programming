fbc3ec5be4b58107670fb1fd62826a16213c277e
chimebukanian <chimebukaanyanwu@gmail.com>
2022-03-16 14:37:23 -0700

    s

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * jack_bauer - Print every minute of the day.
+ */
+
+void jack_bauer(void)
+{
+	int h1, h2, m1, m2;
+
+	for (h1 = '0'; h1 < '3'; h1++)
+	{
+		for (h2 = '0'; h2 <= '9'; h2++)
+		{
+			if (!(h1 == '2' && h2 >= '4'))
+			{
+				for (m1 = '0'; m1 < '6'; m1++)
+				{
+					for (m2 = '0'; m2 <= '9'; m2++)
+					{
+						putchar(h1);
+						putchar(h2);
+						putchar(':');
+						putchar(m1);
+						putchar(m2);
+						putchar('\n');
+					}
+				}
+			}
+		}
+	}
+}
