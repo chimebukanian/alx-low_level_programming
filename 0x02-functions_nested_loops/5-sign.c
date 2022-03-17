048e42b095c0db4e53c3905c9ff930b3064c4626
chimebukanian <chimebukaanyanwu@gmail.com>
2022-03-16 23:33:34 -0700

    f

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,17 +15,17 @@ int last_digit;
 if (n < 0)
 {
 last_digit = (-1 * (n % 10));
-_putchar (last_digit + '0');
+putchar (last_digit + '0');
 return (last_digit);
 }
 
 else
 {
 last_digit = (n % 10);
-_putchar (last_digit + '0');
+putchar (last_digit + '0');
 return (last_digit);
 }
 
-_putchar('\n');
+putchar('\n');
 
 }
