88afc1a979d5ba794e0a595c8071956be7246f60
chimebukanian <chimebukaanyanwu@gmail.com>
2022-03-21 19:09:34 -0700

    g

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,15 +16,15 @@ unsigned int num = 0;
 do {
 
     if (*s == '-')
-      sign *= -1;
+        sign *= -1;
 
     else if (*s >= '0' && *s <= '9')
-      num = (num * 10) + (*s - '0');
+        num = (num * 10) + (*s - '0');
 
     else if (num > 0)
-      break;
+        break;
 
 } while (*s++);
 
- return (num * sign);
+return (num * sign);
 }
