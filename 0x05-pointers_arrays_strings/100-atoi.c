dc181b4b717c870eebe21620fdefd23723f96c70
chimebukanian <chimebukaanyanwu@gmail.com>
2022-03-21 19:05:03 -0700

    g

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,10 +10,10 @@
 int _atoi(char *s)
 {
 
-  int sign = 1;
-  unsigned int num = 0;
+int sign = 1;
+unsigned int num = 0;
 
-  do {
+do {
 
     if (*s == '-')
       sign *= -1;
@@ -24,7 +24,7 @@ int _atoi(char *s)
     else if (num > 0)
       break;
 
-  } while (*s++);
+} while (*s++);
 
-  return (num * sign);
+ return (num * sign);
 }
