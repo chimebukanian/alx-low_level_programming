08c7198dbfabe29d2db14f70aa067370741ccdbe
chimebukanian <chimebukaanyanwu@gmail.com>
2022-06-09 17:08:13 -0700

    all_done

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -21,8 +21,3 @@ size_t dlistint_len(const dlistint_t *h)
 
 	return (nodes);
 }
-
-/**
- * delete_dnodeint_at_index - deltes a node in a doubly linked list
- * at a given index
- * @head: double pointer to the list
