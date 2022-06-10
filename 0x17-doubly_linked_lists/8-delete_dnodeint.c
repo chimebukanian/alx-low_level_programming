43291ac06f2d9a0bfa037de4f7c2e5d1e4749c4b
chimebukanian <chimebukaanyanwu@gmail.com>
2022-06-09 17:02:00 -0700

    all_done

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * dlistint_len -function returns the number of nodes in a doubly linked list
+ * @h: pointer to the list
+ *
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	if (!h)
+		return (0);
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
+
+/**
+ * delete_dnodeint_at_index - deltes a node in a doubly linked list
+ * at a given index
+ * @head: double pointer to the list
