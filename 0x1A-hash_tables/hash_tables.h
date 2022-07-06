8093aea64ef70fb3bb3ae2e2db7f6fd83a4efd3f
chimebukanian <chimebukaanyanwu@gmail.com>
2022-07-06 06:06:28 -0700

    fixes

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,9 +1,8 @@
-#ifndef HASH_TABLES_H
-~#define HASH_TABLES_H
+#ifndef _HASH_TABLES_H_
+#define _HASH_TABLES_H_
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <stdio.h>
 #include <string.h>
 
 /**
@@ -43,12 +42,6 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 
-void replace_value(hash_node_t **ht, const char *key, const char *value);
-int check_key(hash_node_t *ht, const char *key);
-hash_node_t *add_node(hash_node_t **head, const char *key, const char *value);
-void print_list(hash_node_t *h);
-void free_list(hash_node_t *head);
-
 /**
  * struct shash_node_s - Node of a sorted hash table
  *
@@ -61,11 +54,11 @@ void free_list(hash_node_t *head);
  */
 typedef struct shash_node_s
 {
-	 char *key;
-	 char *value;
-	 struct shash_node_s *next;
-	 struct shash_node_s *sprev;
-	 struct shash_node_s *snext;
+	char *key;
+	char *value;
+	struct shash_node_s *next;
+	struct shash_node_s *sprev;
+	struct shash_node_s *snext;
 } shash_node_t;
 
 /**
@@ -80,10 +73,10 @@ typedef struct shash_node_s
  */
 typedef struct shash_table_s
 {
-	 unsigned long int size;
-	 shash_node_t **array;
-	 shash_node_t *shead;
-	 shash_node_t *stail;
+	unsigned long int size;
+	shash_node_t **array;
+	shash_node_t *shead;
+	shash_node_t *stail;
 } shash_table_t;
 
 shash_table_t *shash_table_create(unsigned long int size);
@@ -93,11 +86,4 @@ void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
 
-int replace_value_s(shash_node_t **ht, const char *key, const char *value);
-int check_key_s(shash_node_t *ht, const char *key);
-shash_node_t *add_node_s(shash_node_t **head,
-	const char *key, const char *value);
-void insert_sort(shash_node_t *node, shash_table_t *ht);
-void free_list_s(shash_node_t *head);
-
-#endif
+#endif /* _HASH_TABLES_H_ */
